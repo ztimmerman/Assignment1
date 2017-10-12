c54a2d7080a31e8e14e3040345ecf31095e697af
Zachary Timmerman <xployalist@gmail.com>
2017-10-12 07:56:58 -0400

    Add SimpleAxe.cpp and SimpleAxe.h
    Created files SimpleAxe.cpp and SimpleAxe.h
    Filled out header file SimpleAxe.h

---
+++ SimpleAxe.h
@@ -0,0 +1,23 @@
+/* File: SimpleAxe.h
+ * Author: Zachary Timmerman
+ * Created 10/12/17
+ */
+
+#include <string>
+#include "Weapon.h"
+
+#ifndef SIMPLEAXE_H
+#define SIMPLEAXE_H
+
+/* Describes behavior of Simple Axe weapon. HP = 60. If armor > 0 && armor < 20,
+ * axe ignores all armor points.
+ */
+
+class SimpleAxe : public Weapon {
+  SimpleAxe : Weapon("Simple axe",60.0) {
+  }
+  virtual ~SimpleAxe()  {};
+  virtual double hit(double armor);
+}
+
+#endif  //SIMPLEAXE_H
