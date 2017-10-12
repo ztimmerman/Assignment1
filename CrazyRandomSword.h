0b531ee325d702b6b25416d719c6b23424fbde91
Zachary Timmerman <xployalist@gmail.com>
2017-10-12 08:31:13 -0400

    Populate CrazyRandomSword.h
    Added all member function declarations to CrazyRandomSword.h

--- CrazyRandomSword.h
+++ CrazyRandomSword.h
@@ -2,3 +2,22 @@
  * Author: Zachary Timmerman
  * Created: 10/12/17
  */
+
+ #include <string>
+ #include <stdlib.h>
+ #include <time.h>
+ #include "Weapon.h"
+
+#ifndef CRAZYRANDOMSWORD_H
+#define CRAZYRANDOMSWORD_H
+
+class CrazyRandomSword : public Weapon  {
+  //default number must be given
+  CrazyRandomSword : Weapon("Random sword",10.0)  {
+  }
+  virtual ~CrazyRandomSword() {};
+  virtual double hit(double armor);
+};
+
+
+#endif
