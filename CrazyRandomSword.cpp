5910c9249e2dd8de7ccd39cea73b7180580f81e0
Zachary Timmerman <xployalist@gmail.com>
2017-10-12 08:39:47 -0400

    Implement CrazyRandomSword::hit
    Implemented hit function, including its random
    functionality.

--- CrazyRandomSword.cpp
+++ CrazyRandomSword.cpp
@@ -2,3 +2,16 @@
  * Author: Zachary Timmerman
  * Created: 10/12/17
  */
+
+ #include "CrazyRandomSword.h"
+
+ double CrazyRandomSword::hit(double armor) {
+   srand(time(NULL));
+   double damage = rand() % 90 + 10.0 - (armor - (rand() %
+    static_cast<int>(floor(armor * 0.5))));
+   if (damage < 0)  {
+     return 0;
+   } else {
+     return damage;
+   }
+ }
