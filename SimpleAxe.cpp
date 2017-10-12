b1658b45a6283cbb493e6d2270b15ed0b548a388
Zachary Timmerman <xployalist@gmail.com>
2017-10-12 08:09:50 -0400

    Update constant values in SimpleAxe.cpp to doubles

--- SimpleAxe.cpp
+++ SimpleAxe.cpp
@@ -7,12 +7,12 @@
 
  double SimpleAxe::hit (double armor) {
    double damage;
-   if (armor > 0 && armor < 20) {
+   if (armor > 0.0 && armor < 20.0) {
      return hitPoints;
    }  else  {
      damage = hitPoints - armor;
-     if (damage < 0)  {
-       return 0;
+     if (damage < 0.0)  {
+       return 0.0;
      } else {
        return damage;
      }
